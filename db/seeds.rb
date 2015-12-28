bb19d1b7d22b13dc835a64f0b633bf5c36e25ca2
BlueAccords <myoldemailname@gmail.com>
2015-12-28 02:13:44 -0800

    added seeding for mod table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,53 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# puts Dir.pwd
+
+require 'json'
+require 'ffaker'
+
+file = open("db/nexustop100_API.json")
+jsonFile = file.read
+
+parsed = JSON.parse(jsonFile)
+
+
+
+# parsed.each do |mod|
+#   puts mod["file_link"]
+# end
+
+puts parsed[0]["file_value"]
+
+#puts jsonFile
+
+	# t.text    "game",              limit: 255
+ #    t.text    "name",              limit: 255
+ #    t.text    "aliases",           limit: 255
+ #    t.boolean "is_utility"
+ #    t.integer "category",          limit: 2
+ #    t.boolean "has_adult_content"
+ #    t.integer "nm_id",             limit: 4
+ #    t.integer "ws_id",             limit: 4
+ #    t.integer "ll_id",             limit: 4
+
+
+4.times do |n|
+  #name  = FFaker::Name.last_name,
+  # email = "example-#{n+1}@railstutorial.org"
+  # password = "password"
+  rand_category = rand(255).to_i
+  file_name = parsed[n]["file_value"]
+  nm_link = parsed[n]["file_link"]
+  rand_nm_id = /\d+/.match(nm_link)
+  # Mod.create!( mod_id: n,
+  # 			 game: "Skyrim",
+  # 			 name:  file_name,
+  #              is_utility: false,
+  #              category: rand_category,
+  #              has_adult_content: false,
+  #              nm_id: rand_nm_id
+  #            )
+end
+
